e0684818358edd21a8e6ea23e5266e80b57def14
Ioannis Ntallas <Ioannis.NTALLAS@intrasoft-intl.com>
2020-03-13 12:34:53 +0200

    Bootstrap some pet types.

--- petclinic-web/src/main/java/com/springframework/petclinic/bootstrap/DataLoader.java
+++ petclinic-web/src/main/java/com/springframework/petclinic/bootstrap/DataLoader.java
@@ -1,8 +1,11 @@
 package com.springframework.petclinic.bootstrap;
 
 import com.springframework.petclinic.model.Owner;
+import com.springframework.petclinic.model.Pet;
+import com.springframework.petclinic.model.PetType;
 import com.springframework.petclinic.model.Vet;
 import com.springframework.petclinic.services.OwnerService;
+import com.springframework.petclinic.services.PetTypeService;
 import com.springframework.petclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -13,16 +16,26 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
     //Autowired not needed.
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         // Constructor injection.
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
 
         // Create some test owners.
         Owner owner1 = new Owner();
