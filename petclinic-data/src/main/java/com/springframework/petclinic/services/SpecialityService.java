72b50ec1f9e22cf346c4948742a6193957c71696
Ioannis Ntallas <Ioannis.NTALLAS@intrasoft-intl.com>
2020-03-16 14:37:41 +0200

    Add specialilty service.

---
+++ petclinic-data/src/main/java/com/springframework/petclinic/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package com.springframework.petclinic.services;
+
+import com.springframework.petclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
---
+++ petclinic-data/src/main/java/com/springframework/petclinic/services/map/SpecialityMapService.java
@@ -0,0 +1,34 @@
+package com.springframework.petclinic.services.map;
+
+import com.springframework.petclinic.model.Speciality;
+import com.springframework.petclinic.services.SpecialityService;
+
+import java.util.Set;
+
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialityService {
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+}
